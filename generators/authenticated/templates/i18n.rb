20d7f29a55f4517bfe13c8359abfd6964450dbac
Diego Carrion <dc.rec1@gmail.com>
2008-10-26 13:24:15 -0200

    new paths should be added to the I18n.load_path without removing Rails defaults

--- generators/authenticated/templates/i18n.rb
+++ generators/authenticated/templates/i18n.rb
@@ -1,4 +1,2 @@
 I18n.default_locale = "<%= $rest_auth_default_locale %>"
-Dir["config/locales/*"].each do |file_name|
-  I18n.load_path << file_name
-end
+I18n.load_path += Dir.glob("config/locales/*")
--- generators/authenticated/templates/spec/models/i18n_spec.rb
+++ generators/authenticated/templates/spec/models/i18n_spec.rb
@@ -4,4 +4,8 @@ describe "I18n" do
     I18n.default_locale.should_not be_empty
   end
   
+  it "should not remove Rails locales default paths" do
+    I18n.t('datetime.distance_in_words.half_a_minute', :locale => "en-US").should_not include('translation missing')
+  end
+  
 end
