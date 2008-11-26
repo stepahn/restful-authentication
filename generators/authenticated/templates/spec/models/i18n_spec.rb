29741dbcbb7f0b0d5fd2bc890dcfea7a2f364310
Diego Carrion <dc.rec1@gmail.com>
2008-11-25 22:11:17 -0200

    no need to test the load_path because it is not being edited anymore

--- generators/authenticated/templates/spec/models/i18n_spec.rb
+++ generators/authenticated/templates/spec/models/i18n_spec.rb
@@ -4,8 +4,4 @@ describe "I18n" do
     I18n.default_locale.should_not be_empty
   end
   
-  it "should not remove Rails locales default paths" do
-    I18n.t('datetime.distance_in_words.half_a_minute', :locale => "en-US").should_not include('translation missing')
-  end
-  
 end
